fb2dfd9a4f7f28ed0ae2d4eb9c2733cfb6840932
John B. Lee <j.benlin.lee@gmail.com>
2013-09-22 15:05:26 -0400

    Pull Toggl request into separate function

--- scripts/background.js
+++ scripts/background.js
@@ -1,7 +1,19 @@
-chrome.app.runtime.onLaunched.addListener(function() {
+var $togglEndpoint = 'https://www.toggl.com/api/v8';
+var $togglAuth = btoa('bac9be7567e9d2b37596db427c43c9f1:api_token');
+
+togglRequest = function(method,api,callback,data) {
 	var xhr = new XMLHttpRequest();
-	xhr.open('GET','https://www.toggl.com/api/v8/me');
-	xhr.setRequestHeader('Authorization','Basic YmFjOWJlNzU2N2U5ZDJiMzc1OTZkYjQyN2M0M2M5ZjE6YXBpX3Rva2Vu');
-	xhr.onload = function() { console.log(JSON.parse(xhr.responseText)); };
-	xhr.send();
+	xhr.open(method,$togglEndpoint + api);
+	xhr.setRequestHeader('Authorization','Basic ' + $togglAuth);
+	xhr.onload = function() { callback(xhr.responseText); };
+	
+	if (data !== undefined) {
+		xhr.send(data);
+	} else {
+		xhr.send();
+	}
+}
+
+chrome.app.runtime.onLaunched.addListener(function() {
+	togglRequest('GET','/me', function(resp) { console.log(JSON.parse(resp)); });
 })
