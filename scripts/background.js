e643893124010c0884ddb9a304a00cf3143c5e94
John B. Lee <j.benlin.lee@gmail.com>
2013-09-22 16:10:01 -0400

    Stubbed out API request and added message handler

--- scripts/background.js
+++ scripts/background.js
@@ -5,7 +5,7 @@ togglRequest = function(method,api,callback,data) {
 	var xhr = new XMLHttpRequest();
 	xhr.open(method,$togglEndpoint + api);
 	xhr.setRequestHeader('Authorization','Basic ' + $togglAuth);
-	xhr.onload = function() { callback(xhr.responseText); };
+	xhr.onload = function() { callback(JSON.parse(xhr.responseText)); };
 	
 	if (data !== undefined) {
 		xhr.send(data);
@@ -14,6 +14,47 @@ togglRequest = function(method,api,callback,data) {
 	}
 }
 
+togglEntries = function(start,stop,callback) {
+	var date_start = new Date(start);
+	var date_stop = new Date(stop);
+	var call = '/time_entries?start_date=' + encodeURIComponent(date_start.toISOString()) + '&stop_date=' + encodeURIComponent(date_stop.toISOString());
+	console.log('Fetching entry list: start=' + date_start.toISOString() + ' stop=' + date_stop.toISOString());
+	togglRequest('GET', call, callback);
+}
+
+togglWorkspaces = function(callback) {
+	console.log('Fetching workspaces list');
+}
+
+togglProjects = function(callback) {
+	console.log('Fetching projects list');
+}
+
+togglTags = function(callback) {
+	console.log('Fetching tag list');
+}
+
+handleMessage = function(msg,sender,callback) {
+	console.log('Got message of type ' + msg.type !=== undefined ? msg.type : 'undefined');
+	switch(msg.type) {
+		case 'entries':
+			togglEntries(msg.start, msg.stop, callback);
+			break;
+		case 'workspaces':
+			togglWorkspaces(callback);
+			break;
+		case 'projects':
+			togglProjects(callback);
+			break;
+		case 'tags':
+			togglTags(callback);
+			break;
+		default:
+			break;
+	}
+}
+
 chrome.app.runtime.onLaunched.addListener(function() {
 	togglRequest('GET','/me', function(resp) { console.log(JSON.parse(resp)); });
+	chrome.runtime.onMessage.addListener(handleMessage);
 })
